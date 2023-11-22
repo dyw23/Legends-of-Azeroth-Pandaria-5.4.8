409548dfe347a3b3d053a9e8298bd1a286ca76ea
leelf00 <leelf_00@126.com>
2023-11-22 11:41:00 +0800

    [Db/Pandria] Fix hardcoded database name in sql file

--- sql/updates/master/world/2023_11_21_world_01.sql
+++ sql/updates/master/world/2023_11_21_world_01.sql
@@ -1,3 +1,4 @@
+-- 
 UPDATE `quest_template` SET `OfferRewardText`='It\'s good to see you aggain, $n.$B$BThis is the pool of the skunk, as I\'m sure you noticed.$B$BOver the many ages of Shen-zin Su, animals have died in some of these magical pools. Through their deaths, their spirits were infused into the waters, and anyone touching those waters will take their form.$B$BThere are several cursed pools here, some more dangerous than others.' WHERE `Id`=29521;
 UPDATE `quest_template` SET `RequestItemsText`='Telling you of my strength means nothing. I must show you.' WHERE `Id`=29662;
 UPDATE `quest_template` SET `RequestItemsText`='Have you done as I asked of you?' WHERE `Id`=29661;
@@ -15,7 +16,8 @@ UPDATE `quest_template` SET `OfferRewardText`='Shu\'s desires are not so diffren
 UPDATE `quest_template` SET `OfferRewardText`='The water pincers are strange creatures. They seem to be drawn to still water.' WHERE `Id`=29666;
 UPDATE `quest_template` SET `OfferRewardText`='It looks like you have his attention, $n. I think he likes you.' WHERE `Id`=29678;
 UPDATE `quest_template` SET `OfferRewardText`='I have to admit, that looked pretty fun from my angle.$B$BAnd you seem to have made a new friend.' WHERE `Id`=29679;
-UPDATE `quest_template` SET `OfferRewardText`='$n! I wondered when I would see you again!$B$BWell, the plus side is that finding Wugou, the spirit of earth, was easy enough. Waking him... not so much. I think he\'s going to wake quite sore from all the pushing and hitting I\'ve done trying to rouse him.' WHERE `Id`=29680;UPDATE `sf_world`.`quest_template` SET `RequestItemsText`='We need to kill them down to the very last one! We can leave that one alive I suppose, but if there\'s more than one, our troubles are going to start all over again!' WHERE  `Id`=29769;
+UPDATE `quest_template` SET `OfferRewardText`='$n! I wondered when I would see you again!$B$BWell, the plus side is that finding Wugou, the spirit of earth, was easy enough. Waking him... not so much. I think he\'s going to wake quite sore from all the pushing and hitting I\'ve done trying to rouse him.' WHERE `Id`=29680;
+UPDATE `quest_template` SET `RequestItemsText`='We need to kill them down to the very last one! We can leave that one alive I suppose, but if there\'s more than one, our troubles are going to start all over again!' WHERE  `Id`=29769;
 UPDATE `quest_template` SET `RequestItemsText`='We need to kill them down to the very last one! We can leave that one alive I suppose, but if there\'s more than one, our troubles are going to start all over again!' WHERE `Id`=29769;
 UPDATE `quest_template` SET `RequestItemsText`='Ever since Wugou, the spirit of earth, fell asleep, the dirt is drying out and it\'s getting harder and harder to get new seeds to sprout. We need to hold on to any food we can, or the island will have hard times ahead.' WHERE `Id`=29770;
 
