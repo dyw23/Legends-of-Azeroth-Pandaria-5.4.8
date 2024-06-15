4949a8a0a14abb6ea7a84a3623b6b6f34b70318e
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-06-15 08:24:43 -0500

    fix some issues in blood elf starting zone

--- sql/updates/master/world/2024_06_14_00.sql
+++ sql/updates/master/world/2024_06_14_00.sql
@@ -1,8 +1,8 @@
--- Add column QuestObjectiveId
+# Add column QuestObjectiveId
 ALTER TABLE `quest_poi`
 ADD COLUMN IF NOT EXISTS `QuestObjectiveId` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `objIndex`;
 
--- Try to populate with reasonable quest objective data
+# Try to populate with reasonable quest objective data
 update quest_poi
     inner join
     (
@@ -13,4 +13,22 @@ update quest_poi
     ) objective
     on quest_poi.questId = objective.questId
 set QuestObjectiveId = objective.id
-where objIndex >= 0 # ignore turn-in quest POIs
+where objIndex >= 0; # ignore turn-in quest POIs
+
+# Quest 8325 - Reclaiming Sunstrider Isle
+# fix line break in offer reward text
+set @ENTRY := 8325;
+update quest_template set OfferRewardText = 'You have successfully completed your first task; for that, you are to be congratulated.  Such success gives me faith that you will turn out better than those young blood elves who fail to heed the lessons of their masters.  Continued success will be rewarded - not only with knowledge, but also with tangible rewards as well.$B$BYour work here, however, is not finished.  There is much more to learn, my young friend... '
+where id = @ENTRY;
+
+# Well Watcher Solanian
+# fix position
+set @ENTRY := 15295;
+update creature set position_x = 10376.4, position_y = -6406.69, position_z = 49.798, orientation = 3.58665 where id = @ENTRY;
+
+# Quest 8330 - Solanian's Belongings
+# fix quest poi objective/index
+set @ENTRY := 8330;
+update quest_poi set objIndex = 0, QuestObjectiveId = 256353 where questId = @ENTRY and id = 1;
+update quest_poi set objIndex = 1, QuestObjectiveId = 256354 where questId = @ENTRY and id = 2;
+update quest_poi set objIndex = 2, QuestObjectiveId = 256355 where questId = @ENTRY and id = 3;
