092b22b77c5a3565d7c35ed60e8d0a0195be849f
lee <leelf_00@126.com>
2024-08-26 22:14:10 +0800

    Update Weather.cpp

--- src/server/game/Weather/Weather.cpp
+++ src/server/game/Weather/Weather.cpp
@@ -207,10 +207,6 @@ void Weather::SendFineWeatherUpdateToPlayer(Player* player)
 /// Send the new weather to all players in the zone
 bool Weather::UpdateWeather()
 {
-    Player* player = sWorld->FindPlayerInZone(m_zone);
-    if (!player)
-        return false;
-
     ///- Send the weather packet to all players in this zone
     if (m_grade >= 1)
         m_grade = 0.9999f;
